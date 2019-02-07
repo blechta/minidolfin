b4462c61e7840ea49965d968bdb8bb2c6435b5a3
Chris Richardson <chris@bpi.cam.ac.uk>
2019-02-07 11:11:07 +0000

    Add 3D elasticity demo

--- demo/elasticity_3d.py
+++ demo/elasticity_3d.py
@@ -3,14 +3,14 @@
 #
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 
-
+import pyamg
 import ufl
 import numpy
 import matplotlib.pyplot as plt
 import scipy.sparse.linalg
 import timeit
 
-from minidolfin.meshing import build_unit_square_mesh
+from minidolfin.meshing import build_unit_cube_mesh
 from minidolfin.dofmap import build_dofmap
 from minidolfin.assembling import assemble
 from minidolfin.bcs import bc_apply
@@ -18,10 +18,10 @@ from minidolfin.plot import plot
 
 
 # UFL form
-element = ufl.VectorElement("P", ufl.triangle, 1)
+element = ufl.VectorElement("P", ufl.tetrahedron, 1)
 u, v = ufl.TrialFunction(element), ufl.TestFunction(element)
 
-E = 1.0e9
+E = 1.0
 nu = 0.25
 mu = E / (2.0 * (1.0 + nu))
 lmbda = E * nu / ((1.0 + nu) * (1.0 - 2.0 * nu))
@@ -39,8 +39,8 @@ def sigma(v):
 a = ufl.inner(sigma(u), epsilon(v))*ufl.dx
 
 # Build mesh
-n = 50
-mesh = build_unit_square_mesh(n, n)
+n = 20
+mesh = build_unit_cube_mesh(n, n, n)
 tdim = mesh.reference_cell.get_dimension()
 print('Number cells: {}'.format(mesh.num_entities(tdim)))
 
@@ -56,20 +56,44 @@ A = assemble(dofmap, a, {'scalar_type': scalar})
 t += timeit.default_timer()
 print('Assembly time a: {}'.format(t))
 
-b = numpy.zeros(A.shape[0])
-x = numpy.zeros(A.shape[1])
+# Null space
+B = numpy.zeros((A.shape[0], 6), dtype=A.dtype)
+# Translation
+B[0::3, 0] = 1
+B[1::3, 1] = 1
+B[2::3, 2] = 1
+# Rotation
+B[0::3, 3] = 1
+B[1::3, 3] = -1
+B[1::3, 4] = 1
+B[2::3, 4] = -1
+B[2::3, 5] = 1
+B[0::3, 5] = -1
+# B = scipy.linalg.orth(B)
+
+b = numpy.zeros(A.shape[0], dtype=A.dtype)
+x = numpy.zeros(A.shape[1], dtype=A.dtype)
 
 # Set some BCs - fix bottom edge, and move top corner
-bc_dofs = list(range(n * 2 + 2)) + [len(b) - 2, len(b) - 1]
+bc_dofs = list(range((n + 1) * (n+1) * 3)) + [len(b) - 3, len(b) - 2, len(b) - 1]
 bc_vals = numpy.zeros_like(bc_dofs, dtype=float)
-bc_vals[-1] = 0.01
+bc_vals[-2] = 0.01
 
 bc_apply(bc_dofs, bc_vals, A, b)
 
 t = -timeit.default_timer()
-x = scipy.sparse.linalg.spsolve(A, b)
+ml = pyamg.smoothed_aggregation_solver(A, B)
+print(ml)
+
+res = []
+x = ml.solve(b, residuals=res, tol=numpy.finfo(A.dtype).eps, accel='cg')
+# x = scipy.sparse.linalg.spsolve(A, b)
 t += timeit.default_timer()
 print('Solve time: {}'.format(t))
+
+plt.semilogy(res, marker='o', label='SA')
+plt.legend()
+plt.show()
 
 res = b - A*x
 print("residual: ", numpy.linalg.norm(res), res.max(), res.min())
@@ -77,13 +101,11 @@ print("residual: ", numpy.linalg.norm(res), res.max(), res.min())
 # Plotting...
 
 # Deform mesh by displacement
-x = x.reshape(-1, 2)
-mesh.vertices += x*5
+# x = x.reshape(-1, 3)
+# mesh.vertices += x*5
 
 # Get magnitude of displacement
-xmag = numpy.sqrt(x[:, 0]**2 + x[:, 1]**2)
+# xmag = numpy.sqrt(x[:, 0]**2 + x[:, 1]**2)
 
-plot(mesh, xmag)
-plt.show()
-
-print(x)
+# plot(mesh, xmag)
+# plt.show()
